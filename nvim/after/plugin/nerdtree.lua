3bc5d58217b7ab4ae126b9067a1fdfe90fa64038
erykrybak <71080609+erykrybak@users.noreply.github.com>
2024-06-17 12:54:28 +0200

    Delete nvim directory

--- nvim/after/plugin/nerdtree.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<leader>t", function()
-    vim.cmd("NERDTreeToggle")
-end)
--- nvim/lua/dydyr/init.lua
+++
@@ -1,5 +0,0 @@
-require("dydyr.remap")
-require("dydyr.colors")
-require("dydyr.set")
-vim.api.nvim_set_option('clipboard', 'unnamedplus')
-
