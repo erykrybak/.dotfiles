08ee2616fe87382e36bab61c2e877b2fabb14090
erykrybak <71080609+erykrybak@users.noreply.github.com>
2024-06-17 12:56:29 +0200

    Neovim configuration with lsp support
    Treesitter, neo-tree, autopairs, auto-tags, harpoon, lualine, telescope, undotree

---
+++ nvim/lua/keymap.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+vim.keymap.set('n', '<C-Left>', '<C-w>h', {})
+vim.keymap.set('n', '<C-Down>', '<C-w>j', {})
+vim.keymap.set('n', '<C-Up>', '<C-w>k', {})
+vim.keymap.set('n', '<C-Right>', '<C-w>l', {})
