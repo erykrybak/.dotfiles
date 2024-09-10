06b51c98afffcc5621df388ef85423c11cc16eb9
erykrybak <71080609+erykrybak@users.noreply.github.com>
2024-09-10 08:20:56 +0200

    Update keymap.lua

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -5,3 +5,14 @@ vim.keymap.set('n', '<C-Left>', '<C-w>h', {})
 vim.keymap.set('n', '<C-Down>', '<C-w>j', {})
 vim.keymap.set('n', '<C-Up>', '<C-w>k', {})
 vim.keymap.set('n', '<C-Right>', '<C-w>l', {})
+
+vim.keymap.set('n', '<leader>r', function()
+    local filename = vim.fn.expand('%:p')
+    if filename == '' then
+        print('No file to compile')
+        return
+    end
+    vim.cmd('below split')
+    vim.cmd.terminal()
+    vim.api.nvim_chan_send(vim.bo.channel, "clear && g++ " .. filename .. " && ./a.out && rm a.out\r")
+end, { noremap = true, silent = true })
